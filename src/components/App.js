6b68e99137aff3281df1384ea55296f894545226
Sunky Wang <wac@tradeshift.com>
2018-03-26 23:13:09 +0800

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+    render() {
+        return <div>
+            <h1>Hello World</h1>
+            <h2>Sunky</h2>
+            <h2>Sunky</h2>
+        </div>;
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.[hash:8].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "index.[contenthash:8].css",
+            allChunks: true
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template:'src/index.html'
+        })
+    ]
+};
